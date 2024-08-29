3d14c3848900449a5fa89486e8f12ee18a9e3206
adelson70 <bittencourtadelson1@gmail.com>
2024-08-29 20:05:11 -0300

    recebendo o nome para enviar ao backend

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,18 @@
-console.log('deu certo')
+// OBTENDO ELEMENTO BOTÃO QUE ADICIONA O NOME DA PESSOA
+const botao_adicionar_nome = document.getElementById('adicionarNome')
+
+// EVENTO PARA AÇÃO QUANDO CLICADO NO BOTÃO DE ADICIONAR O NOME DA PESSOA
+botao_adicionar_nome.addEventListener('click', function(){
+    // CAPTURA O NOME DA PESSOA
+    const nome_pessoa = document.getElementById('nomeOracao').value
+
+    // CASO NÃO TENHA DIGITADO O NOME
+    if (nome_pessoa.lenght === undefined){
+        alert('Digite seu nome!')
+    }
+
+    // CASO TENHA DIGITADO O NOME
+    else {
+        alert(`Ola ${nome_pessoa}!`)
+    }
+})
