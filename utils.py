8191b14f9ea188b463b09df8eb8bfd6abe495c2b
adelson70 <bittencourtadelson1@gmail.com>
2024-08-29 19:56:58 -0300

    função para adicionar nome de oração ao db

--- utils.py
+++ utils.py
@@ -4,3 +4,17 @@ from database import conectarDB
 # FUNÇÃO PARA GERAR UM TOKEN DA SESSÃO DO COOKIE
 def gerar_secret_key():
     return secrets.token_hex(32)
+
+# ADICIONAR NOME DE ORAÇÃO
+def adicionarNome(nome):
+    try:
+        conexao, cursor = conectarDB()
+
+        cursor.execute('INSERT INTO pessoas (nome) VALUES (?)',(nome,))
+
+        conexao.commit()
+
+        return 'success'
+
+    except Exception as e:
+        print(f'Função adicionarNome diz: {e}')
