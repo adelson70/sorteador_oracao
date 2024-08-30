a0995fa4c27f56ec374e2f8f19dd87fbc8cc5ee7
adelson70 <bittencourtadelson1@gmail.com>
2024-08-29 22:19:35 -0300

    tratamento para caso tente acessar o adm erroneamente

--- routes.py
+++ routes.py
@@ -11,10 +11,15 @@ def configure_app(app):
     # ROTA DO ADM ONDE IRA SORTEAR O NOME
     @app.route('/admin/<token>')
     def admin(token):
-        if token == '211121':
-            return render_template('admin.html')
-        else:
-            return redirect(url_for('main'))
+        try:
+            if token == '211121':
+                return render_template('admin.html')
+            
+            else:
+                return redirect(url_for('main'))
+            
+        except:
+            return render_template('page_not_found.html')
     
     # ROTA PARA ADICIONAR O NOME AO DB
     @app.route('/adicionarNome', methods=['POST'])
@@ -32,3 +37,8 @@ def configure_app(app):
         guardarNomeCookie(nome) if msg == 'success' else None
 
         return jsonify(response)
+    
+    # ROTA DE TRATAMENTO DO ERRO 404
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('page_not_found.html'), 404
