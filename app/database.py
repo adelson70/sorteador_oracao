1487b1c33456aa34e815bea6abc102df432ef50d
adelson70 <bittencourtadelson1@gmail.com>
2024-11-15 15:07:49 -0300

    testando conexao com o banco

--- app/database.py
+++ app/database.py
@@ -1,5 +1,62 @@
 import sqlite3 as sql
 import tinydb as nosql
 
-def teste():
-    return ...
+# conexao banco sql
+def connSQL():
+    conexao = sql.connect("database/oracao.db")
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS sala(
+                   id INTEGER PRIMARY KEY,
+                   token TEXT(6),
+                   nome TEXT(20),
+                   limiteParticipante INTEGER,
+                   dataCriacao DATE,
+                   dataRevelacao DATE,
+                   link TEXT(45),
+                   status TEXT(10),
+                   estado TEXT(10)
+                   )
+                   """)
+    
+    conexao.commit()
+
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS participante(
+                   id INTEGER PRIMARY KEY,
+                   nome TEXT(20),
+                   whatsapp TEXT(13),
+                   email TEXT(45),
+                   salaToken TEXT(6),
+                   FOREIGN KEY (salaToken) REFERENCES sala (token)
+                   )
+                   """)
+    conexao.commit()
+
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS usuario(
+                   id INTEGER PRIMARY KEY,
+                   nome TEXT(64),
+                   senha TEXT(64),
+                   plano TEXT (64)
+                   )
+                   """)
+    conexao.commit()
+
+    return conexao, cursor
+
+# conxao ao noSql
+def connNsql():
+    db = nosql('database/sorteios.json')
+
+    return db
+
+# carregando banco
+if __name__ == '__main__':
+    try:
+        connSQL()
+        print('banco carregado')
+
+    except Exception as e:
+        print('erro ao carregar o banco ',e)
